850ca6a6841260e7328442adc44a7ffd54ca7d72
JayyyyLee <97768904+JayyyyLee@users.noreply.github.com>
2023-01-04 16:20:54 -0800

    Update calCost_Record.R

--- R/simScore/calCost_Record.R
+++ R/simScore/calCost_Record.R
@@ -7,6 +7,40 @@ library(dplyr)
 source("R/simScore/preprocessing.R")
 source("R/simScore/helperFunctions.R")
 
+# input two sub boundary list
+# without trans
+calCostNoTrans <- function(l1,l2, m, order){
+  actions = 0
+  record=data.frame()
+  m=1-m
+  if (length(l1)!=length(l2)){
+    return ("different speakers try again")
+  }  # check speaker num
+  for (s in seq(1,length(l1))){
+    if (nchar(l1[s])!=nchar(l2[s])){
+      return ("different length of elements")
+    }
+  }  # check element length
+
+  cost=0
+  for (s in seq(1,length(l1))){  # for each speacker
+    for (i in seq(1,nchar(l1[s]))){  # for index of element in each list
+      e1 =substring(l1[s],i,i)
+      e2 =substring(l2[s],i,i)
+      if (e1!=e2){
+        cost=cost+(m[which (order == e1),which (order == e2)])
+        actions=actions+1
+        new_row = c("Substitution", e1, e2)
+        record = rbind(record,new_row)
+      }
+    }
+  }
+
+  colnames(record) = c("type", "e1", "e2")
+  return(c(cost, actions,record))
+}
+
+
 parSimV3 <- function(t1,t2, m, order, transCost, max = Inf, costSoFar = 0, cumulActions = 0){
   if(costSoFar < max){
     t1_trailing_sp = str_extract_last(t1, " +") %>% nchar %>% replace_na(0)
@@ -52,7 +86,7 @@ parSimV3 <- function(t1,t2, m, order, transCost, max = Inf, costSoFar = 0, cumul
       }
     }else{
       if (e1==e2){
-        result = parSim1V3(s1,s2,m, order, transCost, max, costSoFar, cumulActions)
+        result = parSimV3(s1,s2,m, order, transCost, max, costSoFar, cumulActions)
       }else{
         if(length(matches) > 0){
           opCost = m[which (order == t1_list[matches[1]]),which (order == t2_list[matches[1]])]
@@ -62,21 +96,21 @@ parSimV3 <- function(t1,t2, m, order, transCost, max = Inf, costSoFar = 0, cumul
           t2_p1 = substring(t2, 1, matches[1] - 1)
           t2_p2 = substring(t2, matches[1] + 1, nchar(t2))
           result =(c(opCost, -cumulActions) + #Because we're adding tgt two operations, there will be an extra copy of cumulActions, which we delete here
-                     parSim1V3(t1_p1,t2_p1,m, order, transCost, max, costSoFar + opCost, cumulActions) +
-                     parSim1V3(t1_p2,t2_p2,m, order, transCost, max, costSoFar + opCost, cumulActions))
+                     parSimV3(t1_p1,t2_p1,m, order, transCost, max, costSoFar + opCost, cumulActions) +
+                     parSimV3(t1_p2,t2_p2,m, order, transCost, max, costSoFar + opCost, cumulActions))
         } else if(e1!=" " & e2!=" "){
           cumulActions = cumulActions + 1
           opCost = m[which (order == e1),which (order == e2)]
-          result =(c(opCost, 0) + parSim1V3(s1,s2,m, order, transCost, max, costSoFar + opCost, cumulActions))
+          result =(c(opCost, 0) + parSimV3(s1,s2,m, order, transCost, max, costSoFar + opCost, cumulActions))
         } else {
           if (!is.na(t1_first_nonsp) & t1_first_nonsp > 1){ #Moving the first char of t2 back or substituting
             #Old condition:  (substring(t2,1,1)!=" " & substring(t2,1,1)!=" ")
             cumulActions = cumulActions + 1
             option1 = c(m[which (order == e1),which (order == e2)], 0) +
-              parSim1V3(s1,s2,m,order, transCost, max,
+              parSimV3(s1,s2,m,order, transCost, max,
                         costSoFar + m[which (order == e1),which (order == e2)], cumulActions)
             option2 = c(transCost[which (order == e1)] * (t1_first_nonsp - 1), 0) +
-              parSim1V3(t1,tback,m, order, transCost,
+              parSimV3(t1,tback,m, order, transCost,
                         max = min(max, costSoFar + option1[1], na.rm = T),
                         costSoFar + transCost[which (order == e1)] * (t1_first_nonsp - 1), cumulActions)
             if(any(is.na(option1)) & any(is.na(option2))){
@@ -89,10 +123,10 @@ parSimV3 <- function(t1,t2, m, order, transCost, max = Inf, costSoFar = 0, cumul
           } else if(!is.na(t2_first_nonsp) & t2_first_nonsp > 1) { #Moving the first nonspace char of t2 front or substituting
             cumulActions = cumulActions + 1
             option1 = c(m[which (order == e1),which (order == e2)], 0) +
-              parSim1V3(s1,s2,m,order, transCost,max,
+              parSimV3(s1,s2,m,order, transCost,max,
                         costSoFar + m[which (order == e1),which (order == e2)], cumulActions)
             option2 = c(transCost[which (order == e2)] * (t2_first_nonsp - 1), 0) +
-              parSim1V3(t1,tfor,m,order, transCost,
+              parSimV3(t1,tfor,m,order, transCost,
                         max = min(max, costSoFar + option1[1], na.rm = T),
                         costSoFar + transCost[which (order == e2)] * (t2_first_nonsp - 1), cumulActions)
             if(any(is.na(option1)) & any(is.na(option2))){
@@ -105,7 +139,7 @@ parSimV3 <- function(t1,t2, m, order, transCost, max = Inf, costSoFar = 0, cumul
           } else {
             cumulActions = cumulActions + 1
             opCost = m[which (order == e1),which (order == e2)]
-            result =(c(opCost, 0) + parSim1V3(s1,s2,m, order, transCost, max, costSoFar + opCost, cumulActions))
+            result =(c(opCost, 0) + parSimV3(s1,s2,m, order, transCost, max, costSoFar + opCost, cumulActions))
           }
         }
       }
@@ -120,40 +154,6 @@ parSimV3 <- function(t1,t2, m, order, transCost, max = Inf, costSoFar = 0, cumul
 }  # input two sub boundary list
 
 
-# input two sub boundary list
-# without trans
-calCostNoTrans <- function(l1,l2, m, order){
-  actions = 0
-  record=data.frame()
-  m=1-m
-  if (length(l1)!=length(l2)){
-    return ("different speakers try again")
-  }  # check speaker num
-  for (s in seq(1,length(l1))){
-    if (nchar(l1[s])!=nchar(l2[s])){
-      return ("different length of elements")
-    }
-  }  # check element length
-
-  cost=0
-  for (s in seq(1,length(l1))){  # for each speacker
-    for (i in seq(1,nchar(l1[s]))){  # for index of element in each list
-      e1 =substring(l1[s],i,i)
-      e2 =substring(l2[s],i,i)
-      if (e1!=e2){
-        cost=cost+(m[which (order == e1),which (order == e2)])
-        actions=actions+1
-        new_row = c("Substitution", e1, e2)
-        record = rbind(record,new_row)
-      }
-    }
-  }
-
-  colnames(record) = c("type", "e1", "e2")
-  return(c(cost, actions,record))
-}
-
-
 #calCost without report - alternative by Ryan
 calCostV2 <- function(l1,l2,m=matrix(data =c(1,0,0,0,0,0,0,
                                               0,1,0,0,0,0,0,
@@ -162,6 +162,8 @@ calCostV2 <- function(l1,l2,m=matrix(data =c(1,0,0,0,0,0,0,
                                               0,0,0,0,1,0,0,
                                               0,0,0,0,0,1,0,
                                               0,0,0,0,0,0,1), nrow=7),order,transCost){
+
+  record = data.frame()
   subCost=1  # pre-set substitution cost
   # transCost=0.5  # pre-set transition cost of one space
   m=1-m #Similaritie sto differences
@@ -188,6 +190,12 @@ calCostV2 <- function(l1,l2,m=matrix(data =c(1,0,0,0,0,0,0,
     substPos = posMatch & sapply(1:nchar(currBlist1), function(x) substring(currBlist1, x, x) != substring(currBlist2, x, x))
     if(length(which(substPos)) > 0){
       cost = cost + sapply(which(substPos), function(x) m[which(currBlistList1[x] == order), which(currBlistList2[x] == order)]) %>% sum
+
+      for (pos in which(substPos)){
+        new_row = c("Substitution", substring(currBlist1, pos, pos), substring(currBlist2, pos, pos))
+        record = rbind(record,new_row)
+      }
+
     }
     actions = actions + sum(posMatch)
 
@@ -252,14 +260,16 @@ calCostV2 <- function(l1,l2,m=matrix(data =c(1,0,0,0,0,0,0,
             actions = actions + 1
           }
         } else {
-          parSimResult = parSim1V3(t1,t2,m,order,transCost)
+          parSimResult = parSimV3(t1,t2,m,order,transCost)
           cost = cost + parSimResult[1]
           actions = actions + parSimResult[2]  # result of cost and record between two fixed boundaries
         }
       }
     }
   }
-  return(c(cost, actions))
+
+  colnames(record) = c("type", "e1", "e2")
+  return(c(cost, actions,record))
 }  # input 2 genBd
 
 
